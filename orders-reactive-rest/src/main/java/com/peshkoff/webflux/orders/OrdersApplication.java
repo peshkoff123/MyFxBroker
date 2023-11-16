786118482d959dc387fa591c614af707d6b4db6d
peshkoff123 <peshkoff@yahoo.com>
2023-11-16 17:25:53 +0200

    delete test image

--- orders-reactive-rest/src/main/java/com/peshkoff/webflux/orders/OrdersApplication.java
+++ orders-reactive-rest/src/main/java/com/peshkoff/webflux/orders/OrdersApplication.java
@@ -47,19 +47,4 @@ public class OrdersApplication {
 
 		SpringApplication.run(OrdersApplication.class, args);
 	}
-/*
-Simplified and incomplete backend for forex brocker company.
-Its just a training project - will never be finished!
-
-Projects goals:
- - practical work with infrastructure/frameworks:
-    - Kafka
-    - AWS
-    - Reactive programming: Reactor, SpringReactive
-    - MongoDB: reactive and nonReactive drivers
- - this is a training project hence business logic and dataModels are simplified considerably
-
-The DesignDocument:
-https://app.diagrams.net/?src=about#Hpeshkoff123%2FMyFxBroker%2Fmain%2FmyBroker.drawio
-* */
 }
